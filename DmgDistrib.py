3efb3085dd55cddf44e43094d10ef3a3614468bd
Codexys <ivandavid57@gmail.com>
2021-09-11 20:10:10 -0300

    Script v2 added more weapons

--- DmgDistrib.py
+++ DmgDistrib.py
@@ -35,8 +35,82 @@ class Character:
                 self.CR = CR + (0.06+0.02*R)
         elif weapon == "Grasscutter":
                 self.baseATK = baseATK + 608
+                self.ER = ER + 0.551
                 self.dATK =  dATK + (ER-1+0.25+0.05*R) * (0.21+0.07*R)/100
-        
+        elif weapon == "DM not Solo":
+                self.baseATK = baseATK + 454
+                self.CR  = CR + 0.368
+                self.dATK = dATK + 0.12 + R * 0.04
+        elif weapon == "DM solo":
+                self.baseATK = baseATK + 454
+                self.CR  = CR + 0.368
+                self.dATK = dATK + 0.18 +0.06 *R
+        elif weapon == "Blackcliff 0 Stack":
+                self.baseATK = baseATK + 510
+                self.dATK = dATK + (0.09 +0.03 *R) * 0
+                self.CDMG = CDMG + 0.551
+        elif weapon == "Blackcliff 1 Stack":
+                self.baseATK = baseATK + 510
+                self.dATK = dATK + (0.09 +0.03 *R) * 1
+                self.CDMG = CDMG + 0.551
+        elif weapon == "Blackcliff 2 Stack":
+                self.baseATK = baseATK + 510
+                self.dATK = dATK + (0.09 +0.03 *R) * 2
+                self.CDMG = CDMG + 0.551            
+        elif weapon == "Blackcliff 3 Stack":
+                self.baseATK = baseATK + 510
+                self.dATK = dATK + (0.09 +0.03 *R) * 3
+                self.CDMG = CDMG + 0.551
+        elif weapon == "CrescentPike":
+                self.baseATK = baseATK + 565
+        elif weapon == "Lithic 1 Liyue":
+                self.baseATK = baseATK + 565
+                self.dATK = dATK + 0.276 + (0.06 + R*0.01)
+                self.CR = CR + (0.02 + 0.01 *R)        
+        elif weapon == "Lithic 2 Liyue":
+                self.baseATK = baseATK + 565
+                self.dATK = dATK + 0.276 + (0.06 + R*0.01)*2
+                self.CR = CR + (0.02 + 0.01 *R)*2
+        elif weapon == "Lithic 3 Liyue":
+                self.baseATK = baseATK + 565
+                self.dATK = dATK + 0.276 + (0.06 + R*0.01)*3
+                self.CR = CR + (0.02 + 0.01 *R)*3
+        elif weapon == "Lithic 4 Liyue":
+                self.baseATK = baseATK + 565
+                self.dATK = dATK + 0.276 + (0.06 + R*0.01)*4
+                self.CR = CR + (0.02 + 0.01 *R)*4
+        elif weapon == "The Catch":
+                self.baseATK = baseATK + 510
+                self.ER = ER + 0.459
+        elif weapon == "Starglitter":
+                self.baseATK = baseATK + 510
+                self.ER = ER + 0.459
+        elif weapon == "Kitain Spear":
+                self.baseATK = baseATK+ 565
+        elif weapon == "Dragonspine Spear":
+                self.baseATK = baseATK + 454
+        elif weapon == "Royal Spear":
+                self.baseATK = baseATK + 565
+                self.dATK = dATK +0.276
+                #I hate it here
+        elif weapon == "Favonius Lance":
+                self.baseATK = baseATK + 565
+                self.ER = ER + 0.306
+        elif weapon == "Dragonsbane Effect Off":
+                self.baseATK = baseATK + 454
+        elif weapon == "Dragonsbane Effect On":
+                self.baseATK = baseATK + 454
+                self.Edmg = Edmg + 0.16 + 0.04 * R
+        elif weapon == "White Tassel":
+                self.baseATK = baseATK + 401
+                self.CR = CR + 0.234
+        elif weapon == "Black Tassel":
+                self.baseATK = baseATK + 354
+        elif weapon == "Halberd":
+                self.baseATK = baseATK + 448
+                self.dATK = dATK + 0.234
+
+
     def substat(self,flatATK,dATK,CR,CDMG) -> None:
         self.dATK = self.dATK + dATK * 5.8/100
         self.flatATK = self.flatATK + flatATK * 19
@@ -61,7 +135,7 @@ class Character:
         
         return  offmulti * aux1
 
-    def PerfectRotation(self,Bdmg,res,resShred,charlvl,enemylvl,defShred,Average):
+    def PerfectRotation(self,Bdmg,res,resShred,charlvl,enemylvl,defShred,Average,ttds,bennet):
         a1 = charlvl + 100
         a2 = enemylvl +100
         defMiti = a1/(a1+a2*(1-defShred))
@@ -74,28 +148,35 @@ class Character:
             CritMulti = (1+self.CDMG*self.CR)
         else:
             CritMulti = (1+self.CDMG)
-        offmulti1 =(self.baseATK * (1 + self.dATK)+ self.flatATK)*CritMulti
+        if bennet == 1 and self.weapon=="HomaBe":    
+            offmulti1 =(self.baseATK * (1 + self.dATK)+ self.flatATK)- (12736 * (1+(0.15+0.05*self.R))+4780)*(1)/100
+        else:
+            offmulti1 =(self.baseATK * (1 + self.dATK)+ self.flatATK)
         hp = 4.0402
         e = 4.5504
-        as1 = [0.05, 0.1, 0.15, 0.2,0.25]
-        acount = [2, 2 , 3, 2, 4]
-
+        as1 = [0.05, 0.1, 0.15, 0.15,0.2, 0.2,0.25]
+        acount = [2, 2 , 2, 1, 1 ,1, 4]
+        buff10 = [1, 1 , 1, 1, 0 ,0, 0]
+        buff12 = [1, 1 , 1, 1, 0 ,0, 0]
         rotation1=0        
         if self.weapon == "PJWS":
                 bonus = (1+self.Edmg+Bdmg+((self.R)*3+9)/100)
-                for i  in range (0,5):
-                    rotation1 += acount[i]*hp*(bonus+as1[i])
-                rotation1 += e * 4 * (bonus-0.952)
         else:
                 bonus = (1+self.Edmg+Bdmg)
-                for i  in range (0,5):
-                    rotation1+= acount[i]*hp*(bonus+as1[i])
-                rotation1 += e * 4 * (bonus-0.952)
-        return  aux1 * rotation1 *offmulti1
+        for i  in range (0,7):
+
+            rotation1 += acount[i]*hp*(bonus+as1[i])*(offmulti1+buff10[i]*ttds*0.48*self.baseATK+buff12[i]*1207*bennet)
+        
+        if self.weapon == "Kitain Spear":
+            rotation1 += (e * 2 * (bonus-0.952+0.045+0.015*self.R))*offmulti1+(e * 2 * (bonus-0.952+0.045+0.015*self.R))*(offmulti1+ttds*0.48*self.baseATK+1207*bennet)
+        else:
+            rotation1 += (e * 2 * (bonus-0.952))*offmulti1+(e * 2 * (bonus-0.952))*(offmulti1+ttds*0.48*self.baseATK+1207*bennet)
+        
+        return  aux1 * rotation1 *CritMulti
 
    
 a = Character(349,"PJWS",2,311,0.466+0.18, 0.242 , 0.5+0.622 , 0.616 , 1+6.48/100*3 )
-def bestbuild(a,submax):
+def bestbuild(a,submax,cratecap,bennet,ttds):
     aux = 0
     holder= []
     index = 0
@@ -114,8 +195,8 @@ def bestbuild(a,submax):
                                     if (a.CR+k*3.9/100)<=1:
                                         y = deepcopy(a)
                                         y.substat(i,j,k,l)
-                                        if y.CR <= 0.8:
-                                            b1 = y.PerfectRotation(0.952,0.1,0,90,90,0,1)
+                                        if y.CR <= cratecap:
+                                            b1 = y.PerfectRotation(0.952,0.1,0,90,90,0,1,ttds,bennet)
                                             if b1 > max:
                                                 max = b1
                                                 maxi = index
@@ -141,4 +222,4 @@ def bestbuild(a,submax):
     print("cdmg",(a.CDMG*100))
     var = [a.weapon+" R"+str(a.R) , e[0][1],e[0][2],e[0][3],e[0][4],(a.baseATK*(a.dATK+1))+a.flatATK,a.CR,a.CDMG,max]
     return var
-result = bestbuild(a,36)
+result = bestbuild(a,36,0.8,0,0)
--- FileBuilder.py
+++ FileBuilder.py
@@ -4,37 +4,49 @@ from copy import deepcopy
 import csv
 
 a = Character(349,"PJWS",2,311,0.466+0.18, 0.242 , 0.5+0.622 , 0.616 , 1+6.48/100*3 )
-weapon = ["PJWS","HomaAb","HomaBe","VortexSh","VortexNSh","Spine","Grasscutter"]
+weapon = ["PJWS","HomaAb","HomaBe","VortexSh","VortexNSh","Spine","Grasscutter","DM not Solo","DM solo","Blackcliff 0 Stack", "Blackcliff 1 Stack", "Blackcliff 2 Stack","Blackcliff 3 Stack","CrescentPike","Lithic 1 Liyue","Lithic 2 Liyue","Lithic 3 Liyue","Lithic 4 Liyue","The Catch","Starglitter","Kitain Spear","Dragonspine Spear","Royal Spear","Favonius Lance","Dragonsbane Effect Off","Dragonsbane Effect On","White Tassel","Black Tassel","Halberd"]
 header = ['Weapon','flatAtk','ATK%','CritRate%','CritDmg%','ATK','Tot Cr%','Tot Cd%','Damage']
-with open ('WeaponsBest.csv','w',encoding='UTF8',newline='') as f:
+
+bennet = 1
+ttds = 1
+cratecap = 1
+with open ('Weapons45Subs.csv','w',encoding='UTF8',newline='') as f:
         writer=csv.writer(f)
         writer.writerow(header)    
-        for j in range (0,7):
+        for j in range (len(weapon)):
             for i in range(1,6):
                 a = Character(349,weapon[j],i,311,0.466+0.18, 0.242 , 0.5+0.622 , 0.616 , 1+6.48/100*3 )
-                var = bestbuild(a,45)
+                var = bestbuild(a,45,cratecap,bennet,ttds)
                 writer.writerow(var)
-with open ('WeaponsMild.csv','w',encoding='UTF8',newline='') as f:
+with open ('Weapons35Subs.csv','w',encoding='UTF8',newline='') as f:
         writer=csv.writer(f)
         writer.writerow(header)    
-        for j in range (0,7):
+        for j in range (len(weapon)):
             for i in range(1,6):
                 a = Character(349,weapon[j],i,311,0.466+0.18, 0.242 , 0.5+0.622 , 0.616 , 1+6.48/100*3 )
-                var = bestbuild(a,30)
+                var = bestbuild(a,35,cratecap,bennet,ttds)
                 writer.writerow(var)            
-with open ('WeaponsEvenMilder.csv','w',encoding='UTF8',newline='') as f:
+with open ('Weapons30Subs.csv','w',encoding='UTF8',newline='') as f:
         writer=csv.writer(f)
         writer.writerow(header)    
-        for j in range (0,7):
+        for j in range (len(weapon)):
             for i in range(1,6):
                 a = Character(349,weapon[j],i,311,0.466+0.18, 0.242 , 0.5+0.622 , 0.616 , 1+6.48/100*3 )
-                var = bestbuild(a,25)
+                var = bestbuild(a,30,cratecap,bennet,ttds)
+                writer.writerow(var)            
+with open ('Weapons25Subs.csv','w',encoding='UTF8',newline='') as f:
+        writer=csv.writer(f)
+        writer.writerow(header)    
+        for j in range (len(weapon)):
+            for i in range(1,6):
+                a = Character(349,weapon[j],i,311,0.466+0.18, 0.242 , 0.5+0.622 , 0.616 , 1+6.48/100*3 )
+                var = bestbuild(a,25,cratecap,bennet,ttds)
                 writer.writerow(var)                  
-with open ('WeaponsWhy.csv','w',encoding='UTF8',newline='') as f:
+with open ('Weapons20.csv','w',encoding='UTF8',newline='') as f:
         writer=csv.writer(f)
         writer.writerow(header)    
-        for j in range (0,7):
+        for j in range (len(weapon)):
             for i in range(1,6):
                 a = Character(349,weapon[j],i,311,0.466+0.18, 0.242 , 0.5+0.622 , 0.616 , 1+6.48/100*3 )
-                var = bestbuild(a,20)
+                var = bestbuild(a,20,cratecap,bennet,ttds)
                 writer.writerow(var)
