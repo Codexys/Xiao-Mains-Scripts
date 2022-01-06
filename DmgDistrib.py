82a688946085b0d23772dd40a8adbb0b82190f85
Codexys <ivandavid57@gmail.com>
2022-01-06 20:01:26 -0300

    V8 Added Calamity and Unstacked PJWS - Also Added Table Builder for ease of format

--- DmgDistrib.py
+++ DmgDistrib.py
@@ -1,5 +1,6 @@
 from copy import deepcopy
 import csv
+from typing import Match
 
 
 class Character:
@@ -13,102 +14,120 @@ class Character:
         self.Edmg = Edmg
         self.ER = ER
         self.weapon = weapon
-        if weapon == "PJWS":
-            self.CR = CR + 0.221
-            self.baseATK = baseATK + 674
-            self.dATK = dATK + 7*(2.5+0.7*R)/100
-        elif weapon == "HomaAb" or weapon == "HomaBe":
-            self.CDMG = CDMG + 0.662
-            self.baseATK = baseATK + 608
-            if weapon == "HomaAb":
-                self.flatATK = flatATK + (12736 * (1+(0.15+0.05*R))+4780)*(0.6+0.2*R)/100
-            else:
-                self.flatATK = flatATK + (12736 * (1+(0.15+0.05*R))+4780)*(1.6+0.2*R)/100
-        elif weapon == "VortexSh" or weapon == "VortexNSh":
-                self.baseATK = baseATK + 608
-                if weapon == "VortexSh":
-                    self.dATK = dATK + 0.496 + (0.03+0.01*R)*5*2
-                else:
-                    self.dATK = dATK + 0.496 + (0.03+0.01*R)*5
-        elif weapon == "Spine":
+       
+        match weapon:
+            case "PJWS Pre-stacked":
+                self.CR = CR + 0.221
                 self.baseATK = baseATK + 674
-                self.CR = CR + (0.06+0.02*R)
-        elif weapon == "Grasscutter":
+                self.dATK = dATK + 7*(2.5+0.7*R)/100
+            case "PJWS Unstacked":
+                self.CR = CR + 0.221
+                self.baseATK = baseATK + 674
+                self.dATK = dATK 
+            case "HomaAb":  
+                self.CDMG = CDMG + 0.662
                 self.baseATK = baseATK + 608
-                self.ER = ER + 0.551
-                self.dATK =  dATK + (ER + 0.551 -1+0.25+0.05*R) * (0.21+0.07*R)
-        elif weapon == "DM not Solo":
-                self.baseATK = baseATK + 454
-                self.CR  = CR + 0.368
-                self.dATK = dATK + 0.12 + R * 0.04
-        elif weapon == "DM solo":
-                self.baseATK = baseATK + 454
-                self.CR  = CR + 0.368
-                self.dATK = dATK + 0.18 +0.06 *R
-        elif weapon == "Blackcliff 0 Stack":
-                self.baseATK = baseATK + 510
-                self.dATK = dATK + (0.09 +0.03 *R) * 0
-                self.CDMG = CDMG + 0.551
-        elif weapon == "Blackcliff 1 Stack":
-                self.baseATK = baseATK + 510
-                self.dATK = dATK + (0.09 +0.03 *R) * 1
-                self.CDMG = CDMG + 0.551
-        elif weapon == "Blackcliff 2 Stack":
-                self.baseATK = baseATK + 510
-                self.dATK = dATK + (0.09 +0.03 *R) * 2
-                self.CDMG = CDMG + 0.551            
-        elif weapon == "Blackcliff 3 Stack":
-                self.baseATK = baseATK + 510
-                self.dATK = dATK + (0.09 +0.03 *R) * 3
-                self.CDMG = CDMG + 0.551
-        elif weapon == "CrescentPike":
-                self.baseATK = baseATK + 565
-        elif weapon == "Lithic 1 Liyue":
-                self.baseATK = baseATK + 565
-                self.dATK = dATK + 0.276 + (0.06 + R*0.01)
-                self.CR = CR + (0.02 + 0.01 *R)        
-        elif weapon == "Lithic 2 Liyue":
-                self.baseATK = baseATK + 565
-                self.dATK = dATK + 0.276 + (0.06 + R*0.01)*2
-                self.CR = CR + (0.02 + 0.01 *R)*2
-        elif weapon == "Lithic 3 Liyue":
-                self.baseATK = baseATK + 565
-                self.dATK = dATK + 0.276 + (0.06 + R*0.01)*3
-                self.CR = CR + (0.02 + 0.01 *R)*3
-        elif weapon == "Lithic 4 Liyue":
-                self.baseATK = baseATK + 565
-                self.dATK = dATK + 0.276 + (0.06 + R*0.01)*4
-                self.CR = CR + (0.02 + 0.01 *R)*4
-        elif weapon == "The Catch":
-                self.baseATK = baseATK + 510
-                self.ER = ER + 0.459
-        elif weapon == "Starglitter":
-                self.baseATK = baseATK + 510
-                self.ER = ER + 0.459
-        elif weapon == "Kitain Spear":
-                self.baseATK = baseATK+ 565
-        elif weapon == "Dragonspine Spear":
-                self.baseATK = baseATK + 454
-        elif weapon == "Royal Spear":
-                self.baseATK = baseATK + 565
-                self.dATK = dATK +0.276
-                #I hate it here
-        elif weapon == "Favonius Lance":
-                self.baseATK = baseATK + 565
-                self.ER = ER + 0.306
-        elif weapon == "Dragonsbane Effect Off":
-                self.baseATK = baseATK + 454
-        elif weapon == "Dragonsbane Effect On":
-                self.baseATK = baseATK + 454
-                self.Edmg = Edmg + 0.16 + 0.04 * R
-        elif weapon == "White Tassel":
-                self.baseATK = baseATK + 401
-                self.CR = CR + 0.234
-        elif weapon == "Black Tassel":
-                self.baseATK = baseATK + 354
-        elif weapon == "Halberd":
-                self.baseATK = baseATK + 448
-                self.dATK = dATK + 0.234
+                self.flatATK = flatATK + (12736 * (1+(0.15+0.05*R))+4780)*(0.6+0.2*R)/100
+            case "HomaBe":
+                self.CDMG = CDMG + 0.662
+                self.baseATK = baseATK + 608
+                self.flatATK = flatATK + (12736 * (1+(0.15+0.05*R))+4780)*(1.6+0.2*R)/100
+            case "VortexSh":
+                    self.baseATK = baseATK + 608
+                    self.dATK = dATK + 0.496 + (0.03+0.01*R)*5*2
+            case "VortexNSh":
+                    self.baseATK = baseATK + 608
+                    self.dATK = dATK + 0.496 + (0.03+0.01*R)*5
+            case "Spine":
+                    self.baseATK = baseATK + 674
+                    self.CR = CR + (0.06+0.02*R)
+            case "Grasscutter":
+                    self.baseATK = baseATK + 608
+                    self.ER = ER + 0.551
+                    self.dATK =  dATK + (ER + 0.551 -1+0.25+0.05*R) * (0.21+0.07*R)
+            case "Calamity Queller PreBuffed":
+                    self.baseATK = baseATK + 741
+                    self.dATK = dATK + 0.165 + (0.024 + 0.008 * R) * 6
+                    self.Edmg = Edmg + 0.09 + 0.03 * R
+            case "Calamity Queller NoPreBuff":
+                    self.baseATK = baseATK + 741
+                    self.dATK = dATK + 0.165 
+                    self.Edmg = Edmg + 0.09 + 0.03 * R
+            case "DM not Solo":
+                    self.baseATK = baseATK + 454
+                    self.CR  = CR + 0.368
+                    self.dATK = dATK + 0.12 + R * 0.04
+            case "DM solo":
+                    self.baseATK = baseATK + 454
+                    self.CR  = CR + 0.368
+                    self.dATK = dATK + 0.18 +0.06 *R
+            case "Blackcliff 0 Stack":
+                    self.baseATK = baseATK + 510
+                    self.dATK = dATK + (0.09 +0.03 *R) * 0
+                    self.CDMG = CDMG + 0.551
+            case "Blackcliff 1 Stack":
+                    self.baseATK = baseATK + 510
+                    self.dATK = dATK + (0.09 +0.03 *R) * 1
+                    self.CDMG = CDMG + 0.551
+            case "Blackcliff 2 Stack":
+                    self.baseATK = baseATK + 510
+                    self.dATK = dATK + (0.09 +0.03 *R) * 2
+                    self.CDMG = CDMG + 0.551            
+            case "Blackcliff 3 Stack":
+                    self.baseATK = baseATK + 510
+                    self.dATK = dATK + (0.09 +0.03 *R) * 3
+                    self.CDMG = CDMG + 0.551
+            case "CrescentPike":
+                    self.baseATK = baseATK + 565
+            case "Lithic 1 Liyue":
+                    self.baseATK = baseATK + 565
+                    self.dATK = dATK + 0.276 + (0.06 + R*0.01)
+                    self.CR = CR + (0.02 + 0.01 *R)        
+            case "Lithic 2 Liyue":
+                    self.baseATK = baseATK + 565
+                    self.dATK = dATK + 0.276 + (0.06 + R*0.01)*2
+                    self.CR = CR + (0.02 + 0.01 *R)*2
+            case "Lithic 3 Liyue":
+                    self.baseATK = baseATK + 565
+                    self.dATK = dATK + 0.276 + (0.06 + R*0.01)*3
+                    self.CR = CR + (0.02 + 0.01 *R)*3
+            case "Lithic 4 Liyue":
+                    self.baseATK = baseATK + 565
+                    self.dATK = dATK + 0.276 + (0.06 + R*0.01)*4
+                    self.CR = CR + (0.02 + 0.01 *R)*4
+            case "The Catch":
+                    self.baseATK = baseATK + 510
+                    self.ER = ER + 0.459
+            case "Starglitter":
+                    self.baseATK = baseATK + 510
+                    self.ER = ER + 0.459
+            case "Kitain Spear":
+                    self.baseATK = baseATK+ 565
+            case "Dragonspine Spear":
+                    self.baseATK = baseATK + 454
+            case "Wavebreaker's Fin":
+                    self.baseATK = baseATK + 620
+                    self.dATK = dATK + 0.138
+            case "Royal Spear":
+                    self.baseATK = baseATK + 565
+                    self.dATK = dATK +0.276
+                    #I hate it here
+            case "Favonius Lance":
+                    self.baseATK = baseATK + 565
+                    self.ER = ER + 0.306
+            case "Dragonsbane Effect Off":
+                    self.baseATK = baseATK + 454
+            case "Dragonsbane Effect On":
+                    self.baseATK = baseATK + 454
+                    self.Edmg = Edmg + 0.16 + 0.04 * R
+            case "White Tassel":
+                    self.baseATK = baseATK + 401
+                    self.CR = CR + 0.234
+            case "Black Tassel":
+                    self.baseATK = baseATK + 354
+            case "Halberd":
+                    self.baseATK = baseATK + 448
+                    self.dATK = dATK + 0.234
 
 
     def substat(self,flatATK,dATK,CR,CDMG) -> None:
@@ -159,22 +178,46 @@ class Character:
         hp = 4.0402
         e = 4.5504
         as1 = [0.05, 0.1, 0.15, 0.15,0.2, 0.2,0.25]
-        acount = [2, 2 , 2, 1, 1 ,1, 2]
+        acount = [2, 3 , 1, 1, 2 ,1, 2]
         buff10 = [1, 1 , 1, 1, 0 ,0, 0]
-        buff12 = [1, 1 , 1, 1, 0 ,0, 0]
+        buff12 = [1, 1 , 1, 1, 1 ,0, 0]
+        CalamityBuff = (0.024 + 0.008 * self.R)
         rotation1=0        
-        if self.weapon == "PJWS":
+        if self.weapon == "PJWS Pre-stacked":
                 bonus = (1+self.Edmg+Bdmg+((self.R)*3+9)/100)
         else:
                 bonus = (1+self.Edmg+Bdmg)
-        for i  in range (0,7):
 
-            rotation1 += acount[i]*hp*(bonus+as1[i])*(offmulti1+buff10[i]*ttds*0.48*self.baseATK+buff12[i]*1202*bennet)
-        
+        for i  in range (0,7):
+            if self.weapon == "Calamity Queller NoPreBuff":
+                if i == 0:
+                    rotation1 += hp*(bonus+as1[i])*(offmulti1+buff10[i]*ttds*0.48*self.baseATK+buff12[i]*1202*bennet+self.baseATK*CalamityBuff*3)
+                    rotation1 += hp*(bonus+as1[i])*(offmulti1+buff10[i]*ttds*0.48*self.baseATK+buff12[i]*1202*bennet+self.baseATK*CalamityBuff*4)
+                elif i == 1:
+                    rotation1 += hp*(bonus+as1[i])*(offmulti1+buff10[i]*ttds*0.48*self.baseATK+buff12[i]*1202*bennet+self.baseATK*CalamityBuff*5)
+                    rotation1 += 2*hp*(bonus+as1[i])*(offmulti1+buff10[i]*ttds*0.48*self.baseATK+buff12[i]*1202*bennet+self.baseATK*CalamityBuff*6)
+                else:
+                    rotation1 += acount[i]*hp*(bonus+as1[i])*(offmulti1+buff10[i]*ttds*0.48*self.baseATK+buff12[i]*1202*bennet+self.baseATK*CalamityBuff*6)
+            elif self.weapon == "PJWS Unstacked":
+                if i == 0:
+                    rotation1 += hp*(bonus+as1[i])*(offmulti1+buff10[i]*ttds*0.48*self.baseATK+buff12[i]*1202*bennet+self.baseATK*(2.5+0.7*self.R)/100*2)
+                    rotation1 += hp*(bonus+as1[i])*(offmulti1+buff10[i]*ttds*0.48*self.baseATK+buff12[i]*1202*bennet+self.baseATK*(2.5+0.7*self.R)/100*3)
+                elif i == 1:
+                    rotation1 += hp*(bonus+as1[i])*(offmulti1+buff10[i]*ttds*0.48*self.baseATK+buff12[i]*1202*bennet+self.baseATK*(2.5+0.7*self.R)/100*4)
+                    rotation1 += hp*(bonus+as1[i])*(offmulti1+buff10[i]*ttds*0.48*self.baseATK+buff12[i]*1202*bennet+self.baseATK*(2.5+0.7*self.R)/100*5)
+                    rotation1 += hp*(bonus+as1[i])*(offmulti1+buff10[i]*ttds*0.48*self.baseATK+buff12[i]*1202*bennet+self.baseATK*(2.5+0.7*self.R)/100*6)
+                else:
+                    rotation1 += acount[i]*hp*(bonus+as1[i]+(self.R*3+9)/100)*(offmulti1+buff10[i]*ttds*0.48*self.baseATK+buff12[i]*1202*bennet+self.baseATK*(2.5+0.7*self.R)/100*7)
+            else:
+                rotation1 += acount[i]*hp*(bonus+as1[i])*(offmulti1+buff10[i]*ttds*0.48*self.baseATK+buff12[i]*1202*bennet)
+
         if self.weapon == "Kitain Spear":
             rotation1 += (e * 2 * (bonus-0.952+0.045+0.015*self.R))*offmulti1+(e * 2 * (bonus-0.952+0.045+0.015*self.R))*(offmulti1+ttds*0.48*self.baseATK+1202*bennet)
         else:
-            rotation1 += (e * 2 * (bonus-0.952))*offmulti1+(e * 2 * (bonus-0.952))*(offmulti1+ttds*0.48*self.baseATK+1202*bennet)
+            if self.weapon == "PJWS Unstacked":
+                rotation1 += (e * 2 * (bonus-0.952))*offmulti1+(e * 2 * (bonus-0.952+(self.R*3+9)/100)*(offmulti1+ttds*0.48*self.baseATK+1202*bennet))
+            else:
+                rotation1 += (e * 2 * (bonus-0.952))*offmulti1+(e * 2 * (bonus-0.952))*(offmulti1+ttds*0.48*self.baseATK+1202*bennet)
         if (self.weapon == "Lithic 4 Liyue") and bennet:
             #Come on, bennet is not from liyue
             return 0
@@ -182,38 +225,52 @@ class Character:
             return  aux1 * rotation1 *CritMulti
 
    
-a = Character(349,"PJWS",2,311,0.466+0.18, 0.242 , 0.5+0.622 , 0.616 , (1 +  6.48/100*3) )
+
 def bestbuild(a,submax,cratecap,bennet,ttds):
     aux = 0
     holder= []
     index = 0
     max = 0
     maxi = 0
+    flex = 0
+    flexM = 0
     header = ['index','flatAtk','ATK%','CritRate%','CritDmg%','Damage']
     with open ('data.csv','w',encoding='UTF8',newline='') as f:
         writer=csv.writer(f)
         writer.writerow(header)
-        for i in range(4,31):
-                for j in range (4,31):
-                    for k in range (5,31):
-                        for l in range (4,31):
-
-                            if (i+j+k+l)==submax:
+        for i in range(0,31):
+            for j in range (0,31):
+                for k in range (0,31):
+                    for l in range (0,31):
+                        if (i+j+k+l)==submax:
+                            if not((i>24) or (j>24) or (k>24) or (l>30)):
                                 if not((i>24) or (j>24) or (k>24) or (l>30)):
-                                    if (a.CR+(k-1)*3.9/100)<=1:
-                                        y = deepcopy(a)
-                                        y.substat(i,j,k,l)
-                                        if y.CR <= cratecap+3.9/100:
-                                            b1 = y.PerfectRotation(0.952,0.1,0,90,90,0,1,ttds,bennet)
-                                            if b1 > max:
-                                                max = b1
-                                                maxi = index
-                                                        
-                                            b2 = [index, i, j, k, l]
-                                            writer.writerow([index, i, j, k, l,b1])
-                                            
-                                            index += 1
-                                            holder.append([b2,b1])                        
+                                    flexCounter = 0
+                                    if i>4:
+                                        flexCounter += (i-4)
+                                    if j>4:
+                                        flexCounter += (j-4)
+                                    if k>4:
+                                        flexCounter += (k-4)
+                                    if l>4:
+                                        flexCounter += (l-5)
+                                    if flexCounter <= 25:
+                                        if (a.CR+(k-1)*3.9/100)<=1:
+                                                y = deepcopy(a)
+                                                y.substat(i,j,k,l)
+                                                if y.CR <= cratecap+3.9/100:
+                                                    b1 = y.PerfectRotation(0.952,0.1,0,90,90,0,1,ttds,bennet)
+                                                    if b1 > max:
+                                                        max = b1
+                                                        maxi = index
+                                                                
+                                                    b2 = [index, i, j, k, l]
+                                                    writer.writerow([index, i, j, k, l,b1])
+                                                    
+                                                    index += 1
+                                                    holder.append([b2,b1])         
+                                else:
+                                    break               
     print("-----------------------------------------------")
     #print(holder)
     print("Best build of substats for single high plunge is: ")
@@ -233,4 +290,3 @@ def bestbuild(a,submax,cratecap,bennet,ttds):
     else:
         var = [a.weapon+" R"+str(a.R) , e[0][1],e[0][2],e[0][3],e[0][4],(a.baseATK*(a.dATK+1))+a.flatATK,a.CR,a.CDMG,max]
     return var
-result = bestbuild(a,36,0.8,0,0)
--- FileBuilderTableMode.py
+++ FileBuilderTableMode.py
@@ -11,25 +11,32 @@ if not os.path.exists(folderpth):
 bennet = 0
 ttds = 0
 cratecap = 1
+#Stats
 #Array shenanigans
+table = [0,0,0,0,0]
 weapon = ["PJWS Pre-stacked","PJWS Unstacked","HomaAb","HomaBe","VortexSh","VortexNSh","Spine","Grasscutter","Calamity Queller PreBuffed","Calamity Queller NoPreBuff","Wavebreaker's Fin","DM not Solo","DM solo","Blackcliff 0 Stack", "Blackcliff 1 Stack", "Blackcliff 2 Stack","Blackcliff 3 Stack","CrescentPike","Lithic 1 Liyue","Lithic 2 Liyue","Lithic 3 Liyue","Lithic 4 Liyue","The Catch","Starglitter","Kitain Spear","Dragonspine Spear","Royal Spear","Favonius Lance","Dragonsbane Effect Off","Dragonsbane Effect On","White Tassel","Black Tassel","Halberd"]
 if bennet or ttds:
     header = ['Weapon','flatAtk','ATK%','CritRate%','CritDmg%','Total Atk Pre Buff',"Total Atk Post Buff",'Tot Cr%','Tot Cd%','Damage']
 else:    
     header = ['Weapon','flatAtk','ATK%','CritRate%','CritDmg%','ATK','Tot Cr%','Tot Cd%','Damage']
+header = ['Weapon','R1','R2','R3','R4','R5']
 buffs = [[0,0],[0,1],[1,0],[1,1]]
 buffsH = ['','TTDS','Bennet','TTDS+Bennet']
 subs = [20,25,30,35,40,42]
 for k in range(4):
     for l in subs:
-        filepth = folderpth + '\Weapons' + str(l) + 'Subs'+str(buffsH[k])+'.csv'
+        filepth = folderpth + '\WeaponsTable' + str(l) + 'Subs'+str(buffsH[k])+'.csv'
         with open (filepth,'w',encoding='UTF8',newline='') as f:
                 writer=csv.writer(f)
                 writer.writerow(header)    
                 for j in range (len(weapon)):
                     for i in range(1,6):
-                        a = Character(349,weapon[j],i,311,0.466+0.18, 0.242 , 0.5+0.622 , 0.616 , 1+6.48/100*3 )
+                        a = Character(349,weapon[j],i,311,0.466+0.18*1, 0.242 , 0.5+0.622 , 0.466+0.15*1 , 1+6.48/100*3 )
                         var = bestbuild(a,l,cratecap,buffs[k][0],buffs[k][1])
-                        writer.writerow(var)
+                        if k == 0:    
+                           table[i-1] = var[8]
+                        else:
+                           table[i-1] = var[9]
+                    writer.writerow([weapon[j]]+table)
     
             
